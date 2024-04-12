a987966bf79e3acd5f40c05fa3641a847d1ec84f
Chino Menin <chinomenin@MacBook-Air-de-Chino.local>
2024-04-12 11:32:44 -0300

    todo cambiado a bears

--- miProyecto/app.js
+++ miProyecto/app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/mercadoLibre', mercadoLibreRouter)
+app.use('/bears', mercadoLibreRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
