7d4bfc7ced2d85922efa94b68a334d7dbcd27620
Chino Menin <chinomenin@MacBook-Air-de-Chino.local>
2024-04-19 11:08:13 -0300

    fotos a profile

--- miProyecto/db/usuarios.js
+++ miProyecto/db/usuarios.js
@@ -7,7 +7,7 @@ let usuarios ={lista: [
     user: "usuario1",
     fecha_nacimiento: "2005-06-29 00:00:00",
     dni: 46982347,
-    foto_perfil: null,
+    foto_perfil: "/images/users/b8566174a0dfa98437920797ceb02f7a.jpg",
     seguidores: 5
   },
   {
@@ -16,7 +16,7 @@ let usuarios ={lista: [
     user: "usuario2",
     fecha_nacimiento: "2005-06-29 00:00:00",
     dni: 46678933,
-    foto_perfil: null,
+    foto_perfil: "/images/users/images-1.jpg",
     seguidores: 7
   },
   {
@@ -25,7 +25,7 @@ let usuarios ={lista: [
     user: "usuario3",
     fecha_nacimiento: "2005-06-29 00:00:00",
     dni: 46786312,
-    foto_perfil: null,
+    foto_perfil: "/images/users/images.jpg",
     seguidores: 25
   },
   {
@@ -34,7 +34,7 @@ let usuarios ={lista: [
     user: "usuario4",
     fecha_nacimiento: "2005-06-29 00:00:00",
     dni: 45679768,
-    foto_perfil: null,
+    foto_perfil: "/images/users/Austin.webp",
     seguidores: 7
   },
   {
@@ -43,7 +43,7 @@ let usuarios ={lista: [
     user: "usuario5",
     fecha_nacimiento: "2005-06-29 00:00:00",
     dni: 46873423,
-    foto_perfil: null,
+    foto_perfil: "/images/users/Tyrone.webp",
     seguidores: 0
   },
 ]};
