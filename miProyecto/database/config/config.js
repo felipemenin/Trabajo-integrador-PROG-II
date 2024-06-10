470daa40c6b090fc464450fd628e23f9d0aed9c5
Chino Menin <chinomenin@MacBook-Air-de-Chino.local>
2024-06-10 11:32:57 -0300

    Arreglado

--- miProyecto/database/config/config.js
+++ miProyecto/database/config/config.js
@@ -1,8 +1,22 @@
-module.exports = {
+module.exports= {
   "development": {
     "username": "root",
-    "password": null,
+    "password": "root",
     "database": "TrabajoIntegradorProg2",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
